4bf2806241fcc0ee6c9b4746ad7d4a3221ccdb95
sgse <sgse.vld@gmail.com>
2023-12-12 22:51:56 +0300

    homework-2. add func: instantiate_from_csv(), string_to_number(). add tests for it

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = str(name)  # название товара
+        self.__name = str(name)  # название товара
         self.price = price  # цена за единицу товара
         self.quantity = quantity  # количество товара в магазине
         self.all.append(self)  # Помещает товар в список обращаемых
@@ -32,3 +35,33 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Одноименный с приватным атрибутом метод.
+        Декоратор property берет метод, и позволяет к нему обращаться как к атрибуту
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name_str):
+        """
+        Сеттер
+        """
+        if len(str(name_str)) > 10:
+            self.__name = str(name_str)[:10]
+        else:
+            self.__name = str(name_str)
+
+    @classmethod
+    def instantiate_from_csv(cls, path_str):
+        with open(path_str, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            if reader.fieldnames[0] == "name" and reader.fieldnames[1] == "price" and reader.fieldnames[2] == "quantity":
+                for i in reader:
+                    cls(i['name'], i['price'], i['quantity'])
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
