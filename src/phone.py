08a985ec00830b1bc688d1231c96c174b475181d
sgse <sgse.vld@gmail.com>
2023-12-21 19:11:10 +0300

    fix (adding a property and setter for number_of_sim)

--- src/phone.py
+++ src/phone.py
@@ -6,13 +6,24 @@ class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
         self.__name = name
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"Phone('{self.__name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if int(value) < 1 or not isinstance(value, int):
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        else:
+            self._number_of_sim = value
+
     def check_sim(self):
-        if int(self.number_of_sim) < 1 or not isinstance(self.number_of_sim, int):
+        if int(self._number_of_sim) < 1 or not isinstance(self._number_of_sim, int):
             return "ValueError: Количество физических SIM-карт должно быть целым числом больше нуля."
         else:
             return None
